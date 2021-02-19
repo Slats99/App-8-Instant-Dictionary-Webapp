392b6a982be44a9a2f8dff69ccf5c2310fb39d8e
Martin Slater <martinhslater"gmail.com>
2021-02-19 16:32:07 +0000

    Created nav menu for home page

---
+++ webapp/dictionary.py
@@ -0,0 +1,40 @@
+import justpy as jp
+import definition
+
+
+class Dictionary:
+    path = "/dictionary"
+
+    @classmethod
+    def serve(cls, req):
+        # Build the web page
+        wp = jp.QuasarPage(tailwind=True)
+        # Create div for the whole page
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen p-24")
+        # create a container for the dictionary function
+        con = jp.Div(a=div, classes="bg-purple-300 p-10 rounded")
+        # two lines of titles
+        jp.Div(a=con, text="Instant English Dictionary", classes="text-4xl m-2")
+        jp.Div(a=con, text="Get the definition of any word instantly as you type", classes="text-lg")
+        # line div creates a holder to put input box in so it can be declared after output div
+        line = jp.Div(a=con, classes="grid grid-cols-2")
+        # box where output is displayed listed before input box so input box can reference it
+        output_div = jp.Div(a=con, classes="m-2 p-2 text-lg border-2 h-40")
+        # box where input is typed
+        input_box = jp.Input(a=line, placeholder="Type in a word here...", outputdiv=output_div,
+                             classes="m-2 bg-gray-200 border-2 border-gray-200 rounded w-64 py-2 px-4 "
+                                     "focus:outline-none focus:border-purple-500 focus:bg-white")
+        # dynamic method which calls get_definition as text is typed
+        input_box.on('input', cls.get_definition)
+
+        return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        """
+        gets the definition of a word using definition.Definition
+        :param: widget is the widget which calls the function
+        :return: none
+        """
+        defined = definition.Definition(widget.value).get()
+        widget.outputdiv.text = " ".join(defined)
--- webapp/home.py
+++ webapp/home.py
@@ -4,13 +4,41 @@ import justpy as jp
 class Home:
     path = "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-        con = jp.Div(a=div, classes="bg-purple-300 m-24 p-10 rounded")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header, classes="bg-purple-700")
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode='left', bordered=True)
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        a_class = "p-2 m-2 text-lg text-purple-400 hover:text-purple-700"
+        jp.A(a=qlist, text="Home", href="/", classes=a_class)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_class)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="About", href="/about", classes=a_class)
+        jp.Br(a=qlist)
+
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text='Instant Dictionary')
+
+        container = jp.QPageContainer(a=layout)
+        con = jp.Div(a=container, classes="bg-purple-300 m-24 p-10 rounded")
         jp.Div(a=con, text="This is the home page", classes="text-4xl m-2")
         jp.Div(a=con, text="""
         This website was created by Martin Slater, 18th February 2021
         using JustPy and Tailwind CSS
         """, classes="text-lg")
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
